2d4433d7c5076f6cddb826fa9528d357342563d5
rjwnz <me@rjw.nz>
2021-08-09 20:47:50 -0400

    Update to write json as text

--- src/request.ts
+++ src/request.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export async function fetchJson(url: string): Promise<String> {
   const response = await axios.get(url)
-  return response.data
+  return JSON.stringify(response.data)
 }
